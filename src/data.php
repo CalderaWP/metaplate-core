2441360eb0759c8740a8816b7b149b2fb094f4c0
Josh Pollock <jpollock412@gmail.com>
2015-03-04 18:00:00 -0500

    fix issue with no metaplates brekaing get metastack

--- src/data.php
+++ src/data.php
@@ -35,23 +35,25 @@ class data {
 		// GET METAPLATEs
 		$metaplates = self::get_registry();
 		$meta_stack = array();
-		foreach( $metaplates as $metaplate_try ){
-			$is_plate = self::get_metaplate( $metaplate_try['id'] );
-			if( !empty( $is_plate['post_type'][$post->post_type] ) ){
-				switch ($is_plate['page_type']) {
-					case 'single':
-						if( is_single() || is_page() ){
+		if ( is_array( $metaplates ) ) {
+			foreach ( $metaplates as $metaplate_try ) {
+				$is_plate = self::get_metaplate( $metaplate_try['id'] );
+				if ( ! empty( $is_plate['post_type'][ $post->post_type ] ) ) {
+					switch ( $is_plate['page_type'] ) {
+						case 'single':
+							if ( is_single() || is_page() ) {
+								$meta_stack[] = $is_plate;
+							}
+							break;
+						case 'archive':
+							if ( ( is_archive() || is_front_page() ) && ( ! is_page() || ! is_single() ) ) {
+								$meta_stack[] = $is_plate;
+							}
+							break;
+						default:
 							$meta_stack[] = $is_plate;
-						}
-						break;
-					case 'archive':
-						if( ( is_archive() || is_front_page() ) && ( !is_page( ) || !is_single( ) ) ){
-							$meta_stack[] = $is_plate;
-						}
-						break;
-					default:
-						$meta_stack[] = $is_plate;
-						break;
+							break;
+					}
 				}
 			}
 		}
